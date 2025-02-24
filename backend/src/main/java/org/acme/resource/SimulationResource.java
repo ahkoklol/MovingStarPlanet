e31e9da0ac44cf8bfc02234fea7ca3519d32f36c
wayne <wayne.wan-chow-wah@etu.umontpellier.fr>
2025-02-24 16:50:57 +0100

    feat: method to start the simulation

--- backend/src/main/java/org/acme/resource/SimulationResource.java
+++ backend/src/main/java/org/acme/resource/SimulationResource.java
@@ -1,7 +1,8 @@
 package org.acme.resource;
 
-import com.example.model.Particle;
-import com.example.service.SimulationService;
+import org.acme.model.Body;
+import org.acme.model.SimulationResult;
+import org.acme.service.SimulationService;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
@@ -15,24 +16,19 @@ public class SimulationResource {
     @Inject
     SimulationService simulationService;
 
-    // Initializes the simulation with default particles.
     @GET
     @Path("/init")
-    public List<Particle> initialize() {
-        return simulationService.getParticles();
+    public List<Body> initialize() {
+        return List.of(
+                new Body(100000, new double[]{0, 0}, new double[]{0, 0}),
+                new Body(1, new double[]{0, 1}, new double[]{0, 0.1}),
+                new Body(1, new double[]{1, 0}, new double[]{0, 0}),
+                new Body(1, new double[]{1, 1}, new double[]{0, -0.1})
+        );
     }
 
-    // Retrieves the current state of all particles in the simulation.
-    @GET
-    public List<Particle> getParticles() {
-        return simulationService.getParticles();
-    }
-
-    // Advances the simulation by a specified time step (here 1).
-    @GET
-    @Path("/step")
-    public List<Particle> step(@QueryParam("dt") @DefaultValue("1") double dt) {
-        simulationService.step(dt);
-        return simulationService.getParticles();
+    @POST
+    public SimulationResult simulate(@QueryParam("totalTime") @DefaultValue("5.0") double totalTime, List<Body> bodies) {
+        return simulationService.simulate(bodies, totalTime);
     }
 }

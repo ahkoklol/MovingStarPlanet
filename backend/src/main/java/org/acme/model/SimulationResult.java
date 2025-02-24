88d26152e2caba322363e06e36af709a31c634f3
wayne <wayne.wan-chow-wah@etu.umontpellier.fr>
2025-02-24 16:37:02 +0100

    feat: class implementation

--- backend/src/main/java/org/acme/model/SimulationResult.java
+++ backend/src/main/java/org/acme/model/SimulationResult.java
@@ -2,4 +2,44 @@ package org.acme.model;
 
 public class SimulationResult {
 
+    import java.util.List;
+
+    public class SimulationResult {
+        private List<double[]> positions;
+        private List<double[]> velocities;
+        private List<Double> times;
+
+        public SimulationResult() {}
+
+        public SimulationResult(List<double[]> positions, List<double[]> velocities, List<Double> times) {
+            this.positions = positions;
+            this.velocities = velocities;
+            this.times = times;
+        }
+
+        public List<double[]> getPositions() {
+            return positions;
+        }
+
+        public void setPositions(List<double[]> positions) {
+            this.positions = positions;
+        }
+
+        public List<double[]> getVelocities() {
+            return velocities;
+        }
+
+        public void setVelocities(List<double[]> velocities) {
+            this.velocities = velocities;
+        }
+
+        public List<Double> getTimes() {
+            return times;
+        }
+
+        public void setTimes(List<Double> times) {
+            this.times = times;
+        }
+    }
+
 }
